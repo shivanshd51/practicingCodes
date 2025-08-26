f5d9ae68c1486cfc3bf8ab6f4105be21ab4364c2
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-08-27 00:19:26 +0530

    insert function

---
+++ PYTHON/file3.py
@@ -0,0 +1,7 @@
+# making insert function
+
+l=[1,2,3,4,5]
+a=int(input("enter the number you want to push:"))
+b=int(input("enter the index where you want to push the element:"))
+l[b]=a
+print("your updated list",l)
