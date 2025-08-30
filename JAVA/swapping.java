77adc086f17f01e82ed1313be042c76d6289afc8
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-08-31 00:07:52 +0530

    java

---
+++ JAVA/swapping.java
@@ -0,0 +1,18 @@
+public class swapping {
+    public static void main(String[] args) {
+        // swapping two numbers  
+         int num1 = 3;
+         int num2 = 4;
+
+         int c = num1;
+         num1=num2;
+         num2=c;
+
+         System.out.println("number 1");
+         System.out.println(num1);
+         System.out.println("number 2");
+         System.out.println(num2);
+
+
+    }
+}
--- PYTHON/file5.py
+++ PYTHON/file5.py
@@ -1,7 +1,2 @@
 # printing name letter by letter 
 
-def checkOddEven(x):
-    if (x%2==0):
-        print("Even")
-
-checkOddEven(4)
