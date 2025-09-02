1db48eba781b785e11da649c318f3bcb51f5e61e
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-09-03 00:16:32 +0530

    LOTN

---
+++ JAVA/LOTN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LOTN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number:");
+        int a = sc.nextInt();
+        System.out.print("Enter the Second Number:");
+        int b = sc.nextInt();
+        System.out.print("Enter the Third Number:");
+        int c = sc.nextInt();
+
+        if((a>b)&&(a>c)&&())
+        
+    }
+}
