67109c182f2407e3059221110cd6447c9d28253a
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-09-03 17:33:00 +0530

    lotn

--- JAVA/LOTN.java
+++ JAVA/LOTN.java
@@ -10,7 +10,30 @@ public class LOTN {
         System.out.print("Enter the Third Number:");
         int c = sc.nextInt();
 
-        if((a>b)&&(a>c)&&())
+        if((a>b)&&(b>c)){
+            System.out.print("the greates value is:");
+            System.out.println(a);
+        }
+        else if((a>c)&&(c>b)){
+            System.out.print("the greates value is:");
+            System.out.println(a);
+        }
+        else if((b>c)&&(c>a)){
+            System.out.print("the greates value is:");
+            System.out.println(b);
+        }
+        else if((b>a)&&(a>c)){
+            System.out.print("the greates value is:");
+            System.out.println(b);
+        }
+        else if((c>a)&&(a>b)){
+            System.out.print("the greates value is:");
+            System.out.println(c);
+        }
+        else if((c>b)&&(b>a)){
+            System.out.print("the greates value is:");
+            System.out.println(c);
+        }
         
     }
 }
